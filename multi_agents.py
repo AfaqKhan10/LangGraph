50c8bdf098c7bf4a0ae08a9269309a34678a313d
Afaq Khan <afaqhussain.khan02@gmail.com>
2025-11-07 11:53:53 +0500

    multi_agents.py

--- multi_agents.py
+++ multi_agents.py
@@ -16,7 +16,6 @@ class State(TypedDict):
     response: str
 
 
-
 # Supervisor: Classifies the query
 def supervisor_agent(state: State):
     prompt = f"""
@@ -34,7 +33,6 @@ def supervisor_agent(state: State):
     print(f"[Supervisor] → {category}")
     return {"category": category}
 
-
 # Worker: Generates the final answer
 def worker_agent(state: State):
     category = state["category"]
@@ -42,17 +40,14 @@ def worker_agent(state: State):
 
     prompts = {
         "tech": f"You are a tech support agent. Answer in 2-3 short sentences, giving clear and simple troubleshooting steps.\nUser query: {query}",
-
         "billing": f"You are a billing support agent. Answer in a polite and helpful way using only 2-3 short sentences.\nUser query: {query}",
-
         "refund": f"You are a refund specialist. Provide a short, honest, and helpful reply (2-3 sentences max).\nUser query: {query}",
-
         "general": f"You are a friendly support assistant. Respond naturally in 1-2 short sentences.\nUser query: {query}"
     }
-
     response = llm.invoke(prompts.get(category, prompts["general"])).content
     print(f"[Worker] → {category} category handled\n")
     return {"response": response}
+
 
 def route_next(state: State) -> Literal["worker", END]:
     return "worker" if state["category"] in ["tech", "billing", "refund", "general"] else END
@@ -60,10 +55,11 @@ def route_next(state: State) -> Literal["worker", END]:
 
 # Build the graph
 graph = StateGraph(State)
+# add nodes
 graph.add_node("supervisor", supervisor_agent)
 graph.add_node("worker", worker_agent)
 graph.set_entry_point("supervisor")
-
+# add edges
 graph.add_conditional_edges("supervisor", route_next)
 graph.add_edge("worker", END)
 app = graph.compile()
@@ -74,7 +70,7 @@ def ask(question: str):
     print("User:", question)
     print("Bot:", result["response"])
 
-# Run demo
 if __name__ == "__main__":
     user_query = input("Enter your question: ")
     ask(user_query)
+
