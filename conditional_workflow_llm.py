b23cc1c6d2b84270a86fcf49c1e698d5772b0e08
Afaq Khan <afaqhussain.khan02@gmail.com>
2025-11-10 16:07:35 +0500

    Add files via upload

---
+++ conditional_workflow_llm.py
@@ -0,0 +1,117 @@
+
+# " SMART CUSTOMER REVIEW ANALYZER "
+
+from langgraph.graph import StateGraph, START, END
+from langchain_groq import ChatGroq
+from typing import TypedDict, Literal
+from pydantic import BaseModel, Field
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+groq_api_key = os.getenv("GROQ_API_KEY")
+model = ChatGroq(temperature=0.1, api_key=groq_api_key, model="llama-3.1-8b-instant")
+
+
+class SentimentSchema(BaseModel):
+    sentiment: Literal["positive", "negative"] = Field(description='Sentiment of the review')
+
+
+class DiagnosisSchema(BaseModel):
+    issue_type: Literal["UX", "Performance", "Bug", "Support", "Other"] = Field(description='The category of issue mentioned in the review')
+    tone: Literal["angry", "frustrated", "disappointed", "calm"] = Field(description='The emotional tone expressed by the user')
+    urgency: Literal["low", "medium", "high"] = Field(description='How urgent or critical the issue appears to be')
+
+
+structured_model = model.with_structured_output(SentimentSchema)
+structured_model2 = model.with_structured_output(DiagnosisSchema)
+
+
+
+
+class ReviewState(TypedDict):
+    review: str
+    sentiment: Literal["positive", "negative"]
+    diagnosis: dict
+    response: str
+
+
+def find_sentiment(state: ReviewState):
+    prompt = f'For the following review find out the sentiment \n {state["review"]}'
+    sentiment = structured_model.invoke(prompt).sentiment
+    return {'sentiment': sentiment}
+
+
+def check_sentiment(state: ReviewState) -> Literal["positive_response", "run_diagnosis"]:
+    if state['sentiment'] == 'positive':
+        return 'positive_response'
+    else:
+        return 'run_diagnosis'
+
+
+
+def positive_response(state: ReviewState):
+    prompt = f"""Write a warm thank-you message in response to this review:
+    \n\n\"{state['review']}\"\n
+Also, kindly ask the user to leave feedback on our website."""    
+    response = model.invoke(prompt).content
+    return {'response': response}
+
+
+
+def run_diagnosis(state: ReviewState):
+    prompt = f"""
+Analyze this negative review and return the result **in valid JSON** strictly matching this schema:
+{{
+    "issue_type": "UX" | "Performance" | "Bug" | "Support" | "Other",
+    "tone": "angry" | "frustrated" | "disappointed" | "calm",
+    "urgency": "low" | "medium" | "high"
+}}
+
+Review:
+{state['review']}
+"""
+    response = structured_model2.invoke(prompt)
+    return {'diagnosis': response.model_dump()}
+
+
+
+def negative_response(state: ReviewState):
+    diagnosis = state['diagnosis']
+    prompt = f"""You are a support assistant.
+The user had a '{diagnosis['issue_type']}' issue, sounded '{diagnosis['tone']}', and marked urgency as '{diagnosis['urgency']}'.
+Write an empathetic, helpful resolution message.
+"""
+    response = model.invoke(prompt).content
+    return {'response': response}
+
+
+
+
+graph = StateGraph(ReviewState)
+
+graph.add_node('find_sentiment', find_sentiment)
+graph.add_node('positive_response', positive_response)
+graph.add_node('run_diagnosis', run_diagnosis)
+graph.add_node('negative_response', negative_response)
+
+graph.add_edge(START, 'find_sentiment')
+
+graph.add_conditional_edges('find_sentiment', check_sentiment)
+
+graph.add_edge('positive_response', END)
+
+graph.add_edge('run_diagnosis', 'negative_response')
+graph.add_edge('negative_response', END)
+
+workflow = graph.compile()
+
+
+
+initial_state={
+    'review':'The app keeps crashing every time I try to open the dashboard.'
+}
+result = workflow.invoke(initial_state)
+print(result)
+
